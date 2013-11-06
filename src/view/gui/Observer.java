7fc59969fed3afc95dd76428e6a95e0c84468909
jimbofagan <jimbofagan@email.arizona.edu>
2013-11-06 10:30:13 -0700

    Create Observer.java

---
+++ src/view/gui/Observer.java
@@ -0,0 +1,15 @@
+package view.gui;
+
+/**
+ * Provides an interface for the GUI to implement
+ * in accordance with the Observer/Observable Design pattern.
+ * 
+ * @author Christopher Chapline
+ * @author Christopher Toepfer
+ * @author David Christy
+ * @author James Fagan
+ */
+public interface Observer 
+{
+	public void update();
+}
