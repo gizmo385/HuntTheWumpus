5819d44c6ce20bc687b50f7ca9e08ff6736271dc
jimbofagan <jimbofagan@email.arizona.edu>
2013-11-06 10:30:39 -0700

    Create Observable.java

---
+++ src/view/gui/Observable.java
@@ -0,0 +1,33 @@
+package view.gui;
+
+import java.util.ArrayList;
+
+/**
+ * Provides an abstract class for model to extend
+ * in accordance with the Observer/Observable Design pattern.
+ * 
+ * @author Christopher Chapline
+ * @author Christopher Toepfer
+ * @author David Christy
+ * @author James Fagan
+ */
+public abstract class Observable 
+{
+	ArrayList<Observer> observers = new ArrayList<Observer>();
+	
+	public void addObserver(Observer newObserver)
+	{
+		observers.add(newObserver);
+	}
+	
+	public boolean removeObserver(Observer observerToRemove)
+	{
+		return observers.remove(observerToRemove);
+	}
+	
+	public void notifyObservers()
+	{
+		for(Observer o: observers)
+			o.update();
+	}
+}
